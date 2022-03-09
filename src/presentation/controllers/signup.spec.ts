92137c431e8ea5dccea3f6b245a8cba92aea13a7
leandroftassara <leandroftassara@gmail.com>
2022-03-09 07:29:18 -0300

    test: ensure SignUpController return 400 if no email is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -20,4 +20,24 @@ describe('SignUp Controller', () => {
       new Error('Missing param: name')
     )
   })
+
+  test('Should return 400 if no name is provided', () => {
+    const sut = new SignUpController()
+
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        // email: 'any_email@gmail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(
+      new Error('Missing param: email')
+    )
+  })
 })
